f7081ec9565622a7bfd93f1ac5618fa7ba92e646
Manish <itzmanish108@gmail.com>
2022-05-28 13:24:09 +0530

    28-may-2022

--- questions_test.go
+++ questions_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func assert(t *testing.T, expected interface{}, output interface{}) {
+	if output != expected {
+		t.Errorf("expected %v but got %v", expected, output)
+	}
+}
+
 func BenchmarkLeetCodeFunctions(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		QueriesOnPointsInsideACircle()
@@ -29,4 +35,28 @@ func TestComplementOfDecimal(t *testing.T) {
 	if out != 4 {
 		t.Errorf("expected 4 but got %v", out)
 	}
+}
+
+func TestCalculateNumberOfStepToReduceANumberToZero(t *testing.T) {
+	out := calculateNumberOfStepToReduceANumberToZero(14)
+	expected := 6
+	assert(t, expected, out)
+	out = calculateNumberOfStepToReduceANumberToZero(8)
+	expected = 4
+	assert(t, expected, out)
+	out = calculateNumberOfStepToReduceANumberToZero(123)
+	expected = 12
+	assert(t, expected, out)
+}
+
+func TestGetMissingNumber(t *testing.T) {
+	out := getMissingNumber([]int{3, 0, 1})
+	expected := 2
+	assert(t, expected, out)
+	out = getMissingNumber([]int{0, 1})
+	expected = 2
+	assert(t, expected, out)
+	out = getMissingNumber([]int{9, 6, 4, 2, 3, 5, 7, 0, 1})
+	expected = 8
+	assert(t, expected, out)
 }
